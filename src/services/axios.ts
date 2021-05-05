15f5cab397f317953ee169140198cb3d24e04df8
Guilherme Pupo <guilh.pupo@gmail.com>
2021-05-04 21:26:20 -0300

    removed token authentication

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,4 @@
 import Axios from "axios";
 export const axios = Axios.create({
-  baseURL: "https://api.github.com",
-  headers: {
-    Authorization: "Bearer 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"
-  }
+  baseURL: "https://api.github.com"
 });
